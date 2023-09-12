6ec60da58d72349b6e38438cc398ac6319370136
wls <1626961806@qq.com>
2023-09-12 12:51:46 +0800

    init

---
+++ wls-dict/src/main/java/plus/wls/dict/project/mapper/DictDataMapper.java
@@ -0,0 +1,16 @@
+package plus.wls.dict.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import plus.wls.dict.api.entity.DictData;
+
+/**
+ * <p>
+ * 字典数据表 Mapper 接口
+ * </p>
+ *
+ * @author wls
+ * @since 2021-09-22
+ */
+public interface DictDataMapper extends BaseMapper<DictData> {
+
+}

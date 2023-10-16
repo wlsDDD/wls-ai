e8739775913f640c217f55b4efc23d567e2e2941
wls <1626961806@qq.com>
2023-10-16 10:29:12 +0800

    init

---
+++ wls-fast/src/main/java/plus/wls/common/utils/job/QuartzDisallowConcurrentExecution.java
@@ -0,0 +1,19 @@
+package plus.wls.common.utils.job;
+
+import plus.wls.project.monitor.entity.SysJob;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+
+/**
+ * 定时任务处理（禁止并发执行）
+ *
+ * @author wls
+ */
+@DisallowConcurrentExecution
+public class QuartzDisallowConcurrentExecution extends AbstractQuartzJob {
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+    
+}

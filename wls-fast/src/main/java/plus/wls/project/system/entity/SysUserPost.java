26e156fcda39c03980a376cdb52b27b227554250
wls <1626961806@qq.com>
2023-12-06 11:10:37 +0800

    init

---
+++ wls-fast/src/main/java/plus/wls/project/system/entity/SysUserPost.java
@@ -0,0 +1,46 @@
+package plus.wls.project.system.entity;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 用户和岗位关联 sys_user_post
+ *
+ * @author wls
+ */
+public class SysUserPost {
+    /**
+     * 用户ID
+     */
+    private Long userId;
+    
+    /**
+     * 岗位ID
+     */
+    private Long postId;
+    
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("userId", getUserId())
+                .append("postId", getPostId())
+                .toString();
+    }
+    
+    public Long getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public Long getPostId() {
+        return postId;
+    }
+    
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+    
+}

ff0bf7e887ac5e37af97ef6b9cf5d74374ec3f45
wls <1626961806@qq.com>
2024-10-03 16:22:51 +0800

    commit

---
+++ wls-shop/app/admin/form/Customview.php
@@ -0,0 +1,203 @@
+<?php
+// +----------------------------------------------------------------------
+// | ShopXO 国内领先企业级B2C免费开源电商系统
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011~2099 http://shopxo.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( https://opensource.org/licenses/mit-license.php )
+// +----------------------------------------------------------------------
+// | Author: Devil
+// +----------------------------------------------------------------------
+namespace app\admin\form;
+
+/**
+ * 自定义页面动态表格
+ * @author  Devil
+ * @blog    http://gong.gg/
+ * @version 1.0.0
+ * @date    2020-06-16
+ * @desc    description
+ */
+class CustomView
+{
+    // 基础条件
+    public $condition_base = [];
+
+    /**
+     * 入口
+     * @author  Devil
+     * @blog    http://gong.gg/
+     * @version 1.0.0
+     * @date    2020-06-16
+     * @desc    description
+     * @param   [array]           $params [输入参数]
+     */
+    public function Run($params = [])
+    {
+        $lang = MyLang('customview.form_table');
+        return [
+            // 基础配置
+            'base' => [
+                'key_field'     => 'id',
+                'status_field'  => 'is_enable',
+                'is_search'     => 1,
+                'is_delete'     => 1,
+                'is_middle'     => 1,
+            ],
+            // 表单配置
+            'form' => [
+                [
+                    'view_type'         => 'checkbox',
+                    'is_checked'        => 0,
+                    'checked_text'      => MyLang('reverse_select_title'),
+                    'not_checked_text'  => MyLang('select_all_title'),
+                    'align'             => 'center',
+                    'width'             => 80,
+                ],
+                [
+                    'label'         => $lang['id'],
+                    'view_type'     => 'field',
+                    'view_key'      => 'id',
+                    'width'         => 110,
+                    'is_copy'       => 1,
+                    'is_sort'       => 1,
+                    'search_config' => [
+                        'form_type'         => 'input',
+                        'where_type'        => '=',
+                    ],
+                ],
+                [
+                    'label'         => $lang['logo'],
+                    'view_type'     => 'images',
+                    'view_key'      => 'logo',
+                    'images_width'  => 30,
+                    'width'         => 80,
+                ],
+                [
+                    'label'             => $lang['name'],
+                    'view_type'         => 'field',
+                    'view_key'          => 'name',
+                    'is_sort'           => 1,
+                    'params_where_name' => 'keywords',
+                    'search_config' => [
+                        'form_type'         => 'input',
+                        'where_type'        => 'like',
+                    ],
+                ],
+                [
+                    'label'              => $lang['is_enable'],
+                    'view_type'          => 'status',
+                    'view_key'           => 'is_enable',
+                    'post_url'           => MyUrl('admin/customview/statusupdate'),
+                    'is_form_su'         => 1,
+                    'align'              => 'center',
+                    'is_sort'            => 1,
+                    'width'              => 130,
+                    'params_where_name'  => 'is_enable',
+                    'search_config'      => [
+                        'form_type'         => 'select',
+                        'where_type'        => 'in',
+                        'data'              => MyConst('common_is_text_list'),
+                        'data_key'          => 'id',
+                        'data_name'         => 'name',
+                        'is_multiple'       => 1,
+                    ],
+                ],
+                [
+                    'label'         => $lang['is_header'],
+                    'view_type'     => 'status',
+                    'view_key'      => 'is_header',
+                    'post_url'      => MyUrl('admin/customview/statusupdate'),
+                    'align'         => 'center',
+                    'is_sort'       => 1,
+                    'width'         => 130,
+                    'search_config' => [
+                        'form_type'         => 'select',
+                        'where_type'        => 'in',
+                        'data'              => MyConst('common_is_text_list'),
+                        'data_key'          => 'id',
+                        'data_name'         => 'name',
+                        'is_multiple'       => 1,
+                    ],
+                ],
+                [
+                    'label'         => $lang['is_footer'],
+                    'view_type'     => 'status',
+                    'view_key'      => 'is_footer',
+                    'post_url'      => MyUrl('admin/customview/statusupdate'),
+                    'align'         => 'center',
+                    'is_sort'       => 1,
+                    'width'         => 130,
+                    'search_config' => [
+                        'form_type'         => 'select',
+                        'where_type'        => 'in',
+                        'data'              => MyConst('common_is_text_list'),
+                        'data_key'          => 'id',
+                        'data_name'         => 'name',
+                        'is_multiple'       => 1,
+                    ],
+                ],
+                [
+                    'label'         => $lang['is_full_screen'],
+                    'view_type'     => 'status',
+                    'view_key'      => 'is_full_screen',
+                    'post_url'      => MyUrl('admin/customview/statusupdate'),
+                    'align'         => 'center',
+                    'is_sort'       => 1,
+                    'width'         => 130,
+                    'search_config' => [
+                        'form_type'         => 'select',
+                        'where_type'        => 'in',
+                        'data'              => MyConst('common_is_text_list'),
+                        'data_key'          => 'id',
+                        'data_name'         => 'name',
+                        'is_multiple'       => 1,
+                    ],
+                ],
+                [
+                    'label'         => $lang['access_count'],
+                    'view_type'     => 'field',
+                    'view_key'      => 'access_count',
+                    'is_sort'       => 1,
+                    'width'         => 160,
+                    'search_config' => [
+                        'form_type'         => 'section',
+                    ],
+                ],
+                [
+                    'label'         => $lang['add_time'],
+                    'view_type'     => 'field',
+                    'view_key'      => 'add_time',
+                    'is_sort'       => 1,
+                    'search_config' => [
+                        'form_type'         => 'datetime',
+                    ],
+                ],
+                [
+                    'label'         => $lang['upd_time'],
+                    'view_type'     => 'field',
+                    'view_key'      => 'upd_time',
+                    'is_sort'       => 1,
+                    'search_config' => [
+                        'form_type'         => 'datetime',
+                    ],
+                ],
+                [
+                    'label'         => MyLang('operate_title'),
+                    'view_type'     => 'operate',
+                    'view_key'      => 'customview/module/operate',
+                    'align'         => 'center',
+                    'fixed'         => 'right',
+                ],
+            ],
+            // 数据配置
+            'data'  => [
+                'table_name'             => 'CustomView',
+                'data_handle'            => 'CustomViewService::CustomViewListHandle',
+                'is_page'                => 1,
+                'is_handle_annex_field'  => 1,
+            ],
+        ];
+    }
+}
+?>
